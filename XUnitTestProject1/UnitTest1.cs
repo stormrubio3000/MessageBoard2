0f3af79ef157c3a9e627ec7605c2306d14885ab9
stormrubio3000 <storm-3000@hotmail.com>
2020-09-18 15:00:04 -0500

    testing

--- XUnitTestProject1/UnitTest1.cs
+++ XUnitTestProject1/UnitTest1.cs
@@ -1,13 +1,28 @@
 using System;
 using Xunit;
+using System.Collections.Generic;
+
+using WebApplication2.Controllers;
+using WebApplication2.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Builder;
 
 namespace XUnitTestProject1
 {
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void Index()
         {
+            Board board = new Board();
+            HomeController controler = new HomeController(board);
+
+            var result = controler.Index(board);
+
+            Assert.NotNull(result);
+
+
 
         }
     }
