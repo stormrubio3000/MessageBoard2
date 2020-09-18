119e5900ab9cabc91dcac2e3e09405fed614a6c2
Darius Vallejo <darius.vallejo@gmail.com>
2020-09-18 14:40:42 -0600

    Adds unit testing

--- XUnitTestProject1/UnitTest1.cs
+++ XUnitTestProject1/UnitTest1.cs
@@ -13,17 +13,28 @@ namespace XUnitTestProject1
     public class UnitTest1
     {
         [Fact]
-        public void Index()
+        public void IndexTestPass()
         {
             Board board = new Board();
             HomeController controler = new HomeController(board);
 
-            var result = controler.Index(board);
+            var result = controler.Index();
 
             Assert.NotNull(result);
+        }
 
+        [Fact]
+        public void MessageTestPass() {
+            Board board = new Board();
+            HomeController controller = new HomeController(board);
+            board.comments.Add(new Comment { comment = "something"});
 
+            ViewResult result = controller.Index(new Comment { comment = "else" }) as ViewResult;
 
+            Board resultBoard = result.ViewData.Model as Board;
+
+            Assert.Equal("something", resultBoard.comments[0].comment);
+            Assert.Equal("else", resultBoard.comments[1].comment);
         }
     }
 }
